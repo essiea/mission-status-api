1c8179bf24b85ba44e837fc02d6913c8052df3f6
EC2 Default User <ec2-user@ip-10-0-0-53.ec2.internal>
2026-04-09 00:48:41 +0000

    Fix Checkov findings and tune scan for demo project

--- terraform/bootstrap/state-backend/main.tf
+++ terraform/bootstrap/state-backend/main.tf
@@ -42,6 +42,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
     id     = "cleanup"
     status = "Enabled"
 
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
     expiration {
       days = 365
     }
